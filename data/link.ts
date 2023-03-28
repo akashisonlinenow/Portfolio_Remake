dd1508080adecd58818eaef9c7540285aacd24b6
Akash Shridharan <akashshridharan2802@gmail.com>
2023-03-28 16:02:13 +0530

    updated link

--- data/link.ts
+++ data/link.ts
@@ -4,6 +4,6 @@ export const resumelink = `https://cdn.jsdelivr.net/gh/${
   process.env.NEXT_PUBLIC_GITHUB_REPO_NAME || "Portfolio_Remake"
 }/data/Resume_new.pdf`;
 
-export const deployedlink = "https://jayesh-singh.vercel.app/";
+export const deployedlink = "https://example.com";
 
 export const socialDesc = `Hey there, I'm Akash Shridharan. Checkout My Portfolio `;
